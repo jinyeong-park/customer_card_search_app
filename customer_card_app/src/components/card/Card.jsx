98a6315a3f0230995f146616ab28ed78e7236db6
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-06-26 13:27:34 -0700

    added customer email

--- customer_card_app/src/components/card/Card.jsx
+++ customer_card_app/src/components/card/Card.jsx
@@ -4,7 +4,9 @@ import './card.styles.css';
 const Card = props => (
   <div className='card-container'>
     <img alt="customer" src={`https://robohash.org/${props.customer.id}?set=set5&size=180x180`} />
-    <h1>{props.customer.name} </h1>
+    <h2>{props.customer.name} </h2>
+    <p>{props.customer.email}</p>
+
   </div>
 );
 
