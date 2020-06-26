4fcc8d32ca8a63c8d7c1c4eed8dece687399cae9
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-06-26 10:52:47 -0700

    added css. made 4 columns

--- monster_card_app/src/App.js
+++ monster_card_app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import CardList from './components/card-list/card-list.component.jsx';
 
 
 class App extends Component {
@@ -20,10 +21,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => (
-          <h1 key={monster.id}> { monster.name } </h1>
-        ))}
+        <CardList name="Jinyeong">
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}> { monster.name } </h1>
+          ))}
+        </CardList>
       </div>
     );
   }
