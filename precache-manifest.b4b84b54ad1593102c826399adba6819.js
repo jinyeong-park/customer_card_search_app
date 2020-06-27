35f1de9e491cf606d1d008c21fc51600908847cf
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-06-26 18:38:22 -0700

    Updates

---
+++ precache-manifest.b4b84b54ad1593102c826399adba6819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75de651a5d35f776cf971f92967c1ac2",
+    "url": "/customer_card_search_app/index.html"
+  },
+  {
+    "revision": "e22eecd2742825a4b1ca",
+    "url": "/customer_card_search_app/static/css/main.77649fd1.chunk.css"
+  },
+  {
+    "revision": "e7d0f8072b6376ec70e9",
+    "url": "/customer_card_search_app/static/js/2.9bc86b1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/customer_card_search_app/static/js/2.9bc86b1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22eecd2742825a4b1ca",
+    "url": "/customer_card_search_app/static/js/main.d70b3c19.chunk.js"
+  },
+  {
+    "revision": "1b56d823b340b4321422",
+    "url": "/customer_card_search_app/static/js/runtime-main.ac59e8d3.js"
+  }
+]);
